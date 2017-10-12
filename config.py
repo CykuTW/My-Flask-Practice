78229950da7ea44132669fdcb9758a96527192d5
CykuTW <rft.tsumiki@gmail.com>
2017-10-12 20:30:29 +0800

    feat(config): add BCRYPT_HANDLE_LONG_PASSWORDS=True

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ SECRET_KEY = '<I_AM_SECRET>'
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@localhost/test-db?charset=utf8mb4"
 # <ENGINE>://<USER>:<PASSWORD>@<HOST>/<DB>?charset=<CHARACTER_SET>
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+BCRYPT_HANDLE_LONG_PASSWORDS = True
