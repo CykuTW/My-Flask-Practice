a6d0aee91587609e447e9ba36b64c724fb683b51
CykuTW <rft.tsumiki@gmail.com>
2017-10-13 09:57:10 +0800

    feat(config): add default config file

---
+++ config_default.py
@@ -0,0 +1,8 @@
+DEBUG = True
+SECRET_KEY = '<I_AM_SECRET>'
+
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@localhost/test-db?charset=utf8mb4"
+# <ENGINE>://<USER>:<PASSWORD>@<HOST>/<DB>?charset=<CHARACTER_SET>
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+BCRYPT_HANDLE_LONG_PASSWORDS = True
