33469d7721bef20be811f8f7ecdac4f4f275cffc
CykuTW <rft.tsumiki@gmail.com>
2017-10-17 11:13:50 +0800

    fix(validations/): remove useless validations package

--- validations/users.py
+++
@@ -1,7 +0,0 @@
-from wtforms import Form, TextField
-from wtforms.validators import Length, DataRequired
-
-
-class LoginValidation(Form):
-    username = TextField('username', [DataRequired(), Length(5, 20)])
-    password = TextField('password', [DataRequired(), Length(5, 20)])
